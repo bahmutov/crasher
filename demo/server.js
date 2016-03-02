2955e630cbec77f17df6b05633297ace2bdb6ef0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-03-02 10:52:47 -0500

    chore(sync): sync crash

--- demo/server.js
+++ demo/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 app.get('/crash', function (req, res) {
-  res.send('We will crash\n')
+  console.log('sync crashing')
+  throw new Error('Sync error')
+  // this code is unreachable
+  res.send('We will crash\n') // eslint-disable-line no-unreachable
 })
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
